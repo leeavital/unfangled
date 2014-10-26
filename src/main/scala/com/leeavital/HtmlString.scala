a5a963435a5fe6a59a2070d631adeee27c822f7b
Lee Avital <leeavital@gmail.com>
2014-10-25 22:36:55 -0700

    Mixin ResponseContent so we can convert HtmlString/JsonString to a response

--- src/main/scala/com/leeavital/HtmlString.scala
+++ src/main/scala/com/leeavital/HtmlString.scala
@@ -5,6 +5,8 @@ package com.leeavital
  * valueclass wrapper for string. Anything that is an "HtmlString" is already safe to be rendered as HTML
  * @param self
  */
-case class HtmlString(self: String) extends AnyVal {
+case class HtmlString(self: String) extends AnyVal with ResponseContent {
   def s = self
+
+  def toResponse: UnfangledResponse = UnfangledResponse.html(this)
 }
--- src/main/scala/com/leeavital/JsonString.scala
+++ src/main/scala/com/leeavital/JsonString.scala
@@ -4,6 +4,8 @@ package com.leeavital
  * valueclass wrapper for string. Anything that is a "JsonString" is already safe to be rendered as JSON
  * @param self
  */
-case class JsonString(self: String) extends AnyVal {
+case class JsonString(self: String) extends AnyVal with ResponseContent {
   def s = self
+
+  def toResponse = UnfangledResponse.json(this)
 }
